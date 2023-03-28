5f1f2563cf384dae0f2790c4061b0631f778e1f0
luojun <853811840@qq.com>
2023-03-28 12:56:01 +0800

    lisu-fifo-4 数据对看做 cw,sf-完全按照fifo_loss 不训练decomp

--- main.py
+++ main.py
@@ -28,17 +28,20 @@ from pytorch_metric_learning.distances import CosineSimilarity
 from pytorch_metric_learning.reducers import MeanReducer
 
 from torch.autograd import Variable
+from torch.nn import functional as F
 
 from torchvision import utils as vutils
 
 np.set_printoptions(threshold=sys.maxsize)
 torch.set_printoptions(profile='full')
+
+
 # torch.backends.cudnn.benchmark = True
 
 
 def gram_matrix(tensor):
     d, h, w = tensor.size()
-    tensor = tensor.view(d, h*w)
+    tensor = tensor.view(d, h * w)
     gram = torch.mm(tensor, tensor.t())
     return gram
 
@@ -52,6 +55,9 @@ def train_and_val(args, Decomp, opt_Decomp, Enhance, opt_Enhance, trainloader, v
     opt_Decomp.zero_grad()
 
     loss_decomp = LossRetinex()
+    kl_loss = torch.nn.KLDivLoss(reduction='batchmean')
+    log_m = torch.nn.LogSoftmax(dim=1)
+    m = torch.nn.Softmax(dim=1)
 
     lr_fpf1 = 1e-3
     lr_fpf2 = 1e-3
@@ -73,7 +79,7 @@ def train_and_val(args, Decomp, opt_Decomp, Enhance, opt_Enhance, trainloader, v
 
     for epoch in range(args.start_epoch, args.num_epochs):
         lr = utils.poly_lr_scheduler(opt_Enhance, 0.001, epoch, max_iter=args.num_epochs)
-        print('epoch: ', epoch, ' lr: ', lr, 'best: ', best_pred)
+        print('epoch:', epoch, 'best:', best_pred, ' lr_enhance:', lr, 'lr_fpf1:', lr_fpf1, 'lr_fpf2:', lr_fpf2)
         loss_record = []
         miou_record = []
 
@@ -83,7 +89,7 @@ def train_and_val(args, Decomp, opt_Decomp, Enhance, opt_Enhance, trainloader, v
         # else:
         #     Decomp.train()
 
-        Decomp.train()
+        Decomp.eval()
         tbar = tqdm(trainloader)
 
         for i, (image, image2, label, name) in enumerate(tbar):  # l lowlight highlight lable
@@ -95,29 +101,30 @@ def train_and_val(args, Decomp, opt_Decomp, Enhance, opt_Enhance, trainloader, v
             I, R = Decomp(image)
             I2, R2 = Decomp(image2)
 
-            I_3 = torch.cat((I, I, I), dim=1)
-            I2_3 = torch.cat((I2, I2, I2), dim=1)
-            recon_low = loss_decomp.recon(R, I_3, image)
-            recon_high = loss_decomp.recon(R2, I2_3, image2)
-            recon_low_2 = loss_decomp.recon(R2, I_3, image)
-            recon_high_2 = loss_decomp.recon(R, I2_3, image2)
-            smooth_i_low = loss_decomp.smooth(image, I)
-            smooth_i_high = loss_decomp.smooth(image2, I2)
-            max_i_low = loss_decomp.max_rgb_loss(image, I)
-            max_i_high = loss_decomp.max_rgb_loss(image2, I2)
+            # I_3 = torch.cat((I, I, I), dim=1)
+            # I2_3 = torch.cat((I2, I2, I2), dim=1)
+            # recon_low = loss_decomp.recon(R, I_3, image)
+            # recon_high = loss_decomp.recon(R2, I2_3, image2)
+            # recon_low_2 = loss_decomp.recon(R2, I_3, image)
+            # recon_high_2 = loss_decomp.recon(R, I2_3, image2)
+            # smooth_i_low = loss_decomp.smooth(image, I)
+            # smooth_i_high = loss_decomp.smooth(image2, I2)
+            # max_i_low = loss_decomp.max_rgb_loss(image, I)
+            # max_i_high = loss_decomp.max_rgb_loss(image2, I2)
+            #
+            # loss1 = recon_low + recon_high + 0.01 * recon_low_2 + 0.01 * recon_high_2 \
+            #         + 0.5 * smooth_i_low + 0.5 * smooth_i_high + \
+            #         0.1 * max_i_low + 0.1 * max_i_high
+            #
+            # # l# 后面epoch 只训练joint net
+            # if epoch > -1:
+            #     opt_Decomp.zero_grad()
+            # else:
+            #     opt_Decomp.zero_grad()
+            #     loss1.backward()
+            #     opt_Decomp.step()
+            # writer.add_scalar("decom_loss", loss1, step)
 
-            loss1 = recon_low + recon_high + 0.01 * recon_low_2 + 0.01 * recon_high_2 \
-                    + 0.5 * smooth_i_low + 0.5 * smooth_i_high + \
-                    0.1 * max_i_low + 0.1 * max_i_high
-
-            # l# 后面epoch 只训练joint net
-            if epoch > 650:
-                opt_Decomp.zero_grad()
-            else:
-                opt_Decomp.zero_grad()
-                loss1.backward()
-                opt_Decomp.step()
-            writer.add_scalar("decom_loss", loss1, step)
             # train fog-pass filtering module
             # freeze the parameters of segmentation network
             # -----------------------------------------------------------
@@ -151,8 +158,8 @@ def train_and_val(args, Decomp, opt_Decomp, Enhance, opt_Enhance, trainloader, v
                 fogpassfilter.train()
                 fogpassfilter_optimizer.zero_grad()
 
-                low_gram = [0]*args.batch_size
-                high_gram = [0]*args.batch_size
+                low_gram = [0] * args.batch_size
+                high_gram = [0] * args.batch_size
                 vector_low_gram = [0] * args.batch_size
                 vector_high_gram = [0] * args.batch_size
                 fog_factor_low = [0] * args.batch_size
@@ -165,10 +172,10 @@ def train_and_val(args, Decomp, opt_Decomp, Enhance, opt_Enhance, trainloader, v
 
                     vector_low_gram[batch_idx] = Variable(low_gram[batch_idx][torch.triu(
                         torch.ones(low_gram[batch_idx].size()[0], low_gram[batch_idx].size()[1])) == 1],
-                                                         requires_grad=True)
+                                                          requires_grad=True)
                     vector_high_gram[batch_idx] = Variable(high_gram[batch_idx][torch.triu(
                         torch.ones(high_gram[batch_idx].size()[0], high_gram[batch_idx].size()[1])) == 1],
-                                                         requires_grad=True)
+                                                           requires_grad=True)
                     # print(vector_low_gram[batch_idx].shape)
 
                     fog_factor_low[batch_idx] = fogpassfilter(vector_low_gram[batch_idx])
@@ -189,6 +196,8 @@ def train_and_val(args, Decomp, opt_Decomp, Enhance, opt_Enhance, trainloader, v
 
                 total_fpf_loss += fog_pass_filter_loss
             total_fpf_loss.backward(retain_graph=False)
+            FogPassFilter1_optimizer.step()
+            FogPassFilter2_optimizer.step()
             # ----------------------------------------------------------
 
             # train segmentation network
@@ -196,6 +205,10 @@ def train_and_val(args, Decomp, opt_Decomp, Enhance, opt_Enhance, trainloader, v
             # l#I R拼接 作为输入
             Enhance.train()
             loss_fsm = 0
+            outfeature_l_logsoftmax = log_m(smap)
+            outfeature_h_softmax = m(smap_n)
+            loss_con = kl_loss(outfeature_l_logsoftmax, outfeature_h_softmax)
+
             for param in Enhance.parameters():
                 param.requires_grad = True
             for param in FogPassFilter1.parameters():
@@ -239,7 +252,8 @@ def train_and_val(args, Decomp, opt_Decomp, Enhance, opt_Enhance, trainloader, v
                 loss_fsm += layer_fsm_loss / 4.
 
             # -----------------------------------------------
-            loss_s = ce_loss(smap_n, label) + args.lambda_fsm*loss_fsm
+            loss_s = ce_loss(smap_n, label) + ce_loss(smap,
+                                                      label) + args.lambda_fsm * loss_fsm + args.lambda_con * loss_con
             loss = loss_s
 
             opt_Enhance.zero_grad()
@@ -247,25 +261,19 @@ def train_and_val(args, Decomp, opt_Decomp, Enhance, opt_Enhance, trainloader, v
             opt_Enhance.step()
             step += 1
 
-            FogPassFilter1_optimizer.step()
-            FogPassFilter2_optimizer.step()
-
-
-            smap_oh = utils.reverse_one_hot(smap)
-            acc, acc_cls, _, _, iou = utils.label_accuracy_score(label.data.cpu().numpy(), smap_oh.data.cpu().numpy(),
-                                                    args.num_classes)  # ll miou iou
-            miou = np.sum(np.nanmean(iou[1:]))
-            # macc = np.sum(np.nanmean(acc_cls[1:]))
-
-            train_loss += loss1.item() + loss.item()
+            # train_loss += loss1.item() + loss.item()
+            train_loss += loss.item()
 
             writer.add_scalar('seg_loss', loss, step)
             writer.add_scalar('train_loss', train_loss, step)
             loss_record.append(train_loss)
 
             # tbar.set_description('TrainLoss: {0:.3} mIoU: {1:.3} S: {2:.3}'.format(
-            tbar.set_description('TrainLoss: {0:.3},mIoU: {1:.3}, seg_loss: {2:.3}, decom_loss:{3:.3}'.format(
-                np.mean(loss_record), miou, loss_s, loss1))
+
+            # tbar.set_description('TrainLoss: {0:.3}, seg_loss: {1:.3}, decom_loss:{2:.3},'
+            #                      .format(np.mean(loss_record), loss_s, loss1))
+            tbar.set_description('TrainLoss: {0:.3}, seg_loss: {1:.3}'
+                                 .format(np.mean(loss_record), loss_s))
 
             # if i % 600 == 0:
             #     I_pow = torch.pow(I_3, 0.1)
@@ -300,7 +308,7 @@ def train_and_val(args, Decomp, opt_Decomp, Enhance, opt_Enhance, trainloader, v
         # writer.add_scalar('loss_epoch_train', float(loss_train_mean), epoch)
 
         if epoch % args.validation_step == 0:
-            acc, macc, miou = val(args, Decomp, opt_Decomp, Enhance, opt_Enhance, trainloader, valloader)
+            acc, macc, miou = val(args, Decomp, opt_Decomp, Enhance, opt_Enhance, trainloader, valloader, epoch)
             writer.add_scalar('acc_val', acc, epoch)
             writer.add_scalar('miou_val', miou, epoch)
 
@@ -321,18 +329,18 @@ def train_and_val(args, Decomp, opt_Decomp, Enhance, opt_Enhance, trainloader, v
                     'optimizer_enhance': opt_Enhance.state_dict(),
                     'best_pred': miou,
                 }, is_best=True, filename='epoch{}best{}.pth'.format(epoch, best_pred))
-            elif epoch > 0 and epoch % 20 == 0:  # 每20轮保存一次
+            elif epoch > 0 and epoch % 50 == 0:  # 每20轮保存一次
                 utils.save_checkpoint({
-                        'epoch': epoch + 1,
-                        'state_dict_decomp': Decomp.state_dict(),
-                        'optimizer_decomp': opt_Decomp.state_dict(),
-                        'state_dict_enhance': Enhance.state_dict(),
-                        'optimizer_enhance': opt_Enhance.state_dict(),
-                        'best_pred': miou,
-                    }, is_best=False, filename='epoch{0}.pth'.format(epoch))
+                    'epoch': epoch + 1,
+                    'state_dict_decomp': Decomp.state_dict(),
+                    'optimizer_decomp': opt_Decomp.state_dict(),
+                    'state_dict_enhance': Enhance.state_dict(),
+                    'optimizer_enhance': opt_Enhance.state_dict(),
+                    'best_pred': miou,
+                }, is_best=False, filename='epoch{0}.pth'.format(epoch))
 
 
-def val(args, Decomp, opt_Decomp, Enhance, opt_Enhance, trainloader, valloader, best_pred=0.0):
+def val(args, Decomp, opt_Decomp, Enhance, opt_Enhance, trainloader, valloader, epoch=0):
     print('start val!')
 
     with torch.no_grad():
@@ -369,14 +377,14 @@ def val(args, Decomp, opt_Decomp, Enhance, opt_Enhance, trainloader, valloader, 
             #
             # loss = loss1 + loss_s
             # loss_record.append(loss.item())
-
+            smap = F.softmax(smap, 1)
             smap_oh = utils.reverse_one_hot(smap)
 
             for l, p in zip(label.data.cpu().numpy(), smap_oh.data.cpu().numpy()):
                 lbls.append(l)
                 preds.append(p)
 
-            if i % 5 == 0:
+            if i % 100 == 0 and epoch % 50 == 0:
                 I_pow = torch.pow(I_3, 0.1)
                 # I_e = I_pow * R_hat
                 I_e = I_pow
@@ -390,18 +398,18 @@ def val(args, Decomp, opt_Decomp, Enhance, opt_Enhance, trainloader, valloader, 
                 lbl = utils.colorize(lbl).numpy()
                 lbl = np.transpose(lbl, (2, 0, 1))
                 # 低光 R, I, R2, I2, -, I_e, label, 分割结果
-                cat_image = np.concatenate([image[0, :, :, :].detach().cpu(), R[0, :, :, :].detach().cpu(),
-                                            I_3[0, :, :, :].detach().cpu(), R2[0, :, :, :].detach().cpu(),
-                                            I2_3[0, :, :, :].detach().cpu(),
-                                            I_e[0, :, :, :].detach().cpu(), lbl, sout], axis=2)
+                cat_image = np.concatenate(
+                    [image[0, :, :, :].detach().cpu(), R[0, :, :, :].detach().cpu(), I_3[0, :, :, :].detach().cpu(),
+                     image2[0, :, :, :].detach().cpu(), R2[0, :, :, :].detach().cpu(), I2_3[0, :, :, :].detach().cpu(),
+                     lbl, sout], axis=2)
                 cat_image = np.transpose(cat_image, (1, 2, 0))
                 cat_image = np.clip(cat_image * 255.0, 0, 255.0).astype('uint8')
 
                 im = Image.fromarray(cat_image)  # 低光，I,R,I_3,R2,I2_3 R_hat,I_e I*R,lbl, sout
                 # 低光 R, I, R2, I2, 恢复R, 恢复低光，label, 分割结果
                 # filepath = os.path.join('/path/to/save/folder', 'val_%d.png' % i)
-                filepath = os.path.join('cat_image', 'val_%d.png' % i)
-                im.save(filepath[:-4] + '.jpg')
+                filepath = os.path.join('cat_image/lisu-fifo-4', f'val{epoch}_{i}.jpg')
+                im.save(filepath)
 
         acc, acc_cls, _, _, iou = utils.label_accuracy_score(lbls, preds, args.num_classes)
         miou = np.sum(np.nanmean(iou[1:]))
@@ -482,7 +490,7 @@ def evaluation(args, Net, Net2, valloader):
                 output = output[0]
 
             else:
-                I, R = Net(image)   # [1, 1, 240, 320]  [1, 3, 240, 320]
+                I, R = Net(image)  # [1, 1, 240, 320]  [1, 3, 240, 320]
                 R_hat, smap, _, _ = Net2(torch.cat((I.detach(), R.detach()), dim=1))  # smap [1, 14, 240, 320] 14-class
 
                 # vutils.save_image(I, 'ilu.png')
@@ -491,7 +499,7 @@ def evaluation(args, Net, Net2, valloader):
                 # print(f'r shape {R.shape} ')
                 # print(f'smap shape {smap.shape} ')
 
-                smap_oh = utils.reverse_one_hot(smap)   # [1, 240, 320]
+                smap_oh = utils.reverse_one_hot(smap)  # [1, 240, 320]
                 # print(f'smap_oh shape {smap_oh.shape} ')
 
                 # if i == 0:
@@ -517,11 +525,10 @@ def evaluation(args, Net, Net2, valloader):
         # print(np.sum(np.nanmean(acc_cls[1:])))  # l mAcc
 
 
-
 def main(argv):
     # basic parameters
     parser = argparse.ArgumentParser()
-    parser.add_argument('--num_epochs', type=int, default=300, help='Number of epochs to train for')
+    parser.add_argument('--num_epochs', type=int, default=1000, help='Number of epochs to train for')
     parser.add_argument('--checkpoint_step', type=int, default=5, help='How often to save checkpoints (epochs)')
     parser.add_argument('--validation_step', type=int, default=1, help='How often to perform validation (epochs)')
     parser.add_argument('--crop_height', type=int, default=576, help='Height of cropped/resized input image to network')
@@ -536,9 +543,10 @@ def main(argv):
     # parser.add_argument('--save_model_path', type=str, default='./checkpoints', help='path to save trained model')
 
     #  ll
-    parser.add_argument('--data_path', type=str, default=r'/mnt/disk2/data/stu010/lj/datasets/LISU/LISU_LLRGBD_real', help='path to your dataset')
-    # parser.add_argument('--pretrained_model_path', type=str, default=r'ckpt/epoch177_pre-fifo_real_best57.6.pth', help='saved model')
-    parser.add_argument('--pretrained_model_path', type=str, default=None, help='saved model')
+    parser.add_argument('--data_path', type=str, default=r'/mnt/disk2/data/stu010/lj/datasets/LISU/LISU_LLRGBD_real',
+                        help='path to your dataset')
+    parser.add_argument('--pretrained_model_path', type=str, default=r'ckpt/save/LISU_LLRGBD_real_best.pth.tar', help='')
+    # parser.add_argument('--pretrained_model_path', type=str, default=None, help='saved model')
     parser.add_argument('--mode', type=str, default='train_and_val', help='train_and_val|output|evaluation')
     parser.add_argument('--cuda', type=str, default='1', help='GPU id used for training')
     parser.add_argument('--save_model_path', type=str, default='ckpt', help='path to save trained model')
@@ -546,7 +554,8 @@ def main(argv):
     # parser.add_argument('--multiple-GPUs', default=True, help='val使用')
     parser.add_argument('--multiple-GPUs', default=False, help='')
     parser.add_argument('--start_epoch', type=int, default=0, help='Start counting epochs from this number')
-    parser.add_argument('--lambda_fsm', type=int, default=0.0000001, help='')
+    parser.add_argument('--lambda_fsm', type=int, default=0.00001, help='')
+    parser.add_argument('--lambda_con', type=int, default=0.0001, help='')
 
     args = parser.parse_args(argv)
 
@@ -558,9 +567,9 @@ def main(argv):
     from torch.utils.data import DataLoader
 
     # random seed
-    # np.random.seed(args.seed)  # cpu vars
-    # torch.manual_seed(args.seed)  # cpu  vars
-    # torch.cuda.manual_seed_all(args.seed)
+    np.random.seed(args.seed)  # cpu vars
+    torch.manual_seed(args.seed)  # cpu  vars
+    torch.cuda.manual_seed_all(args.seed)
 
     Net = models.LISU_DECOMP().cuda()
     Net2 = models.LISU_JOINT().cuda()
@@ -573,13 +582,21 @@ def main(argv):
         if not os.path.isfile(args.pretrained_model_path):
             raise RuntimeError("=> no pretrained model found at '{}'".format(args.pretrained_model_path))
 
+        # checkpoint = torch.load(args.pretrained_model_path)
+        # Net.load_state_dict(checkpoint['state_dict_decomp'], strict=False)
+        # Net2.load_state_dict(checkpoint['state_dict_enhance'], strict=False)
+        # # args.start_epoch = checkpoint['epoch']
+        # opt_Net.load_state_dict(checkpoint['optimizer_decomp'])
+        # opt_Net2.load_state_dict(checkpoint['optimizer_enhance'])
+        # best_pred = checkpoint['best_pred']
+
+        # 只加载decomp
         checkpoint = torch.load(args.pretrained_model_path)
         Net.load_state_dict(checkpoint['state_dict_decomp'])
-        Net2.load_state_dict(checkpoint['state_dict_enhance'])
-        args.start_epoch = checkpoint['epoch']
         opt_Net.load_state_dict(checkpoint['optimizer_decomp'])
-        opt_Net2.load_state_dict(checkpoint['optimizer_enhance'])
-        best_pred = checkpoint['best_pred']
+        best_pred = 0.1
+
+        print('加载decomp')
     else:
         # best_pred = 0.0
         # best_pred = 0.57565
@@ -591,7 +608,7 @@ def main(argv):
     valset = LLRGBD_real(args, mode='val')
     valloader = DataLoader(valset, batch_size=1, shuffle=False, num_workers=0, drop_last=False)
 
-    #trainset = LLRGBD_synthetic(args, mode='train')
+    # trainset = LLRGBD_synthetic(args, mode='train')
     # valset = LLRGBD_synthetic(args, mode='val')
     # == trainloader = DataLoader(trainset, batch_size=12, shuffle=True, num_workers=16, drop_last=True)
     # == valloader = DataLoader(valset, batch_size=1, shuffle=False, num_workers=16, drop_last=False)
